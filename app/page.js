fe94e6737bbdef5b11077dfcd78b319ed2fe3808
Marley <pollardmarlon7@gmail.com>
2024-08-11 16:56:53 -0400

    small changes to main page

--- app/page.js
+++ app/page.js
@@ -4,7 +4,9 @@ import { Box, Stack } from "@mui/material";
 export default function Home() {
   return (
     <Box width="100vw" height="100vh" sx={{}}>
-      <Stack>It's working</Stack>
+      <Stack>
+        <Stack>It's working</Stack>
+      </Stack>
     </Box>
   );
 }
